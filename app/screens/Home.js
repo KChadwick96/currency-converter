23ae988b0d22e46527a7ca5c89cfbd33fdd29acf
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-06-28 20:58:27 +0100

    Initial commit

---
+++ app/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+import { Container } from '../components/Container';
+import { Logo } from '../components/Logo';
+
+export default () => {
+    return (
+        <Container>
+            <StatusBar translucent={false} barStyle='light-content' />
+            <Logo />
+        </Container>
+    )
+}
