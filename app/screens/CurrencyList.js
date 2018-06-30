6b5a768b0967081c620cce2283c54d46c65fae32
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-06-30 22:50:43 +0100

    base/quote currency actions + reducer

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { FlatList, View, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 class CurrencyList extends Component {
 
     static propTypes = {
-        navigation: PropTypes.object
-    }
+        navigation: PropTypes.object,
+        dispatch: PropTypes.func
+    };
 
-    handlePress = () => {
+    handlePress = currency => {
+        const { type } = this.props.navigation.state.params;
+        console.log(type);
+
+        if (type === 'base') {
+            this.props.dispatch(changeBaseCurrency(currency))
+        } else if (type === 'quote') {
+            this.props.dispatch(changeQuoteCurrency(currency));
+        }
+
         this.props.navigation.goBack(null);
-    }
+    };
 
     render() {
         return (
@@ -25,7 +37,7 @@ class CurrencyList extends Component {
                         <ListItem
                             text={item}
                             selected={true}
-                            onPress={this.handlePress}
+                            onPress={() => this.handlePress(item)}
                         />
                     )}
                     keyExtractor={item => item}
@@ -36,4 +48,4 @@ class CurrencyList extends Component {
     }
 }
 
-export default CurrencyList;
+export default connect()(CurrencyList);
